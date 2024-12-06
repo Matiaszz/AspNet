9ee75eb6594dd3d7c0c13cb20025626fd1bd737d
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2024-12-06 18:13:34 -0300

    chore: configuring pages/routes

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+
+// wwwroot = staticfiles
+app.UseStaticFiles();
+
+app.UseRouting();
+app.MapRazorPages();
+
 
 app.Run();
